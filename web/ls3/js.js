863d1326de90f574cad320ebb357ea10224d8031
Daniil Romanshen <vdnv@bk.ru>
2022-08-07 20:42:43 +0600

    finish webpage

---
+++ web/ls3/js.js
@@ -0,0 +1,20 @@
+// import 'animate.css';
+
+function print_contact() {
+    var sk = 'skype: helper'
+    var tg = 'telegram: @helper'
+    var tel = 'tel: 8995345235'
+    alert(sk + '\n' + tg + '\n' + tel)
+}
+
+function show_modal() {
+    modal = document.getElementById("myModal");
+    modal.style.visibility = "visible";
+     
+}
+
+function hide_modal() {
+    modal = document.getElementById("myModal");
+    modal.style.visibility = "hidden";
+     
+}
